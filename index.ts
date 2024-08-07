ec4cd7a1e265314d4cfb34f17c9837224ab6948e
Nanda Sholatul Akbar <nandasholatula@gmail.com>
2024-08-07 17:59:33 +0700

    update

--- index.ts
+++ index.ts
@@ -6,7 +6,6 @@ const app = express();
 const axios = require('axios');
 const PORT = process.env.PORT || 8081;
 
-
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
